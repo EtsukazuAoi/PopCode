91581df0a1b1fe68158c37766c9a053785b73eef
PandaMan <shaefferaelita@gmail.com>
2022-08-23 16:26:11 +0200

    ajout mentions legales

--- main.js
+++ main.js
@@ -5,7 +5,29 @@ var error = 0;
 var listfound = [];
 var languages = {};
 var mentions = {};
-fetch('./ressource/languages.json')
+
+
+function hide(event){
+    event.target.parentNode.style.display = "none";
+}
+
+function closebtn(name) {
+    var closebtn = document.createElement("span");
+    closebtn.className = "close";
+    closebtn.innerHTML = "&times;";
+    closebtn.addEventListener("click", hide);
+    return closebtn;
+}
+
+function init(){
+    // LOADER
+    var loader = document.createElement('div');
+    loader.id = 'loader';
+    loader.style.zIndex = '0';
+    page.appendChild(loader);
+
+    // CONDITIONS 
+    fetch('./ressource/languages.json')
     .then((response) => response.json())
     .then((json) => {
         jsonsize = json.length;
@@ -14,15 +36,17 @@ fetch('./ressource/languages.json')
         }
         var conditions = document.createElement("div");
         conditions.id = "conditions";
-        conditions.style.zIndex = 1000;
-        conditions.innerHTML = json[jsonsize-1].content;
+        conditions.style.display = "none";
+        conditions.innerHTML = "<h1>"+json[jsonsize-1].title+"</h1>"+json[jsonsize-1].content;
+        conditions.appendChild(closebtn("conditions"));
         page.appendChild(conditions);
     });
+    var conditonbtn = document.createElement("span");
+    conditonbtn.className = "conditonbtn";
+    conditonbtn.innerHTML = "Conditions General";
+    conditonbtn.addEventListener("click", function(e) { document.getElementById("conditions").style.display = ""; });
+    page.appendChild(conditonbtn);
 
-/*for(var i ; i<jsonfiles.length-1 ; i++){
-    console.log(jsonfiles[i]);
-}*/
-var loader = document.createElement('div');
-loader.id = 'loader';
-loader.style.zIndex = '0';
-page.appendChild(loader);
+}
+
+init()
