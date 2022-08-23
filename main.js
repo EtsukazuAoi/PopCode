d6b7720e82ecabc95831404ae8058bece074ef24
PandaMan <shaefferaelita@gmail.com>
2022-08-23 11:55:38 +0200

    modified:   index.html 	modified:   main.js 	renamed:    ressource/language/assembly.png -> ressource/assets/img/logos/assembly.png 	renamed:    ressource/language/bash.png -> ressource/assets/img/logos/bash.png 	renamed:    ressource/language/c++.png -> ressource/assets/img/logos/c++.png 	renamed:    ressource/language/c.png -> ressource/assets/img/logos/c.png 	renamed:    ressource/language/clojure.png -> ressource/assets/img/logos/clojure.png 	renamed:    ressource/language/csharp.png -> ressource/assets/img/logos/csharp.png 	renamed:    ressource/language/css.png -> ressource/assets/img/logos/css.png 	renamed:    ressource/language/dart.png -> ressource/assets/img/logos/dart.png 	renamed:    ressource/language/elixir.png -> ressource/assets/img/logos/elixir.png 	renamed:    ressource/language/go.png -> ressource/assets/img/logos/go.png 	renamed:    ressource/language/html.png -> ressource/assets/img/logos/html.png 	renamed:    ressource/language/java.png -> ressource/assets/img/logos/java.png 	renamed:    ressource/language/javascript.png -> ressource/assets/img/logos/javascript.png 	renamed:    ressource/language/kotlin.png -> ressource/assets/img/logos/kotlin.png 	renamed:    ressource/language/objective-c.png -> ressource/assets/img/logos/objective-c.png 	renamed:    ressource/language/php.png -> ressource/assets/img/logos/php.png 	renamed:    ressource/language/powershell.png -> ressource/assets/img/logos/powershell.png 	renamed:    ressource/language/python.png -> ressource/assets/img/logos/python.png 	renamed:    ressource/language/r.png -> ressource/assets/img/logos/r.png 	renamed:    ressource/language/ruby.png -> ressource/assets/img/logos/ruby.png 	renamed:    ressource/language/rust.png -> ressource/assets/img/logos/rust.png 	renamed:    ressource/language/scala.png -> ressource/assets/img/logos/scala.png 	renamed:    ressource/language/sql.png -> ressource/assets/img/logos/sql.png 	renamed:    ressource/language/swift.png -> ressource/assets/img/logos/swift.png 	renamed:    ressource/language/typescript.png -> ressource/assets/img/logos/typescript.png 	renamed:    ressource/language/vba.png -> ressource/assets/img/logos/vba.png 	renamed:    ressource/language/webassembly.png -> ressource/assets/img/logos/webassembly.png 	renamed:    ressource/language/languages.json -> ressource/languages.json 	modified:   style.css

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
+var page = document.getElementById('mainpage');
+page.innerHTML += '<div class="autherdevice">Ce Site web n\'a pas eté concusses pour c\'ettes apparailes</div>';
+var gamesave = JSON.parse(localStorage.getItem("gamesave") || "{}");
+var error = 0;
+var listfound = [];
+var languages = {};
+var mentions = {};
+fetch('./ressource/languages.json')
+    .then((response) => response.json())
+    .then((json) => {
+        jsonsize = json.length;
+        for (var i = 0; i < jsonsize-1; i++) {
+            languages[json[i].name] = {"description": json[i].description, "picture": json[i].picture}
+        }
+        var conditions = document.createElement("div");
+        conditions.id = "conditions";
+        conditions.style.zIndex = 1000;
+        conditions.innerHTML = json[jsonsize-1].content;
+        page.appendChild(conditions);
+    });
 
-
-function mainpage() {
-    var page = document.getElementById('mainpage');
-}
+/*for(var i ; i<jsonfiles.length-1 ; i++){
+    console.log(jsonfiles[i]);
+}*/
+var loader = document.createElement('div');
+loader.id = 'loader';
+loader.style.zIndex = '0';
+page.appendChild(loader);
