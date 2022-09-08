5615453fab4095ee1ae858408dcdcc00959b7b02
PandaMan <shaefferaelita@gmail.com>
2022-09-08 16:27:12 +0200

    ajout loose game page

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ var erreur = 0;
 var gamestate = 0;
 var listfound = [];
 var languages = {};
-var mentions = {};
 var regex = /\r\n|\n|\r/gm;
 
 function startanimation(){
@@ -127,17 +126,12 @@ function checkpourcentage(value){
             }
             tpourcentage = point*100/v.length;
         }
-        // console.log("key: " + key);
-        // console.log("pourcentage: " + tpourcentage);
-        // console.log("point: " + point);
 
         if(tpourcentage > pourcentage){
             pourcentage = tpourcentage;
             keyselected = key;
         }
     }
-    // console.log("key selected: " + keyselected);
-    // console.log("pourcentage max: " + pourcentage);
     if(texttolerance <= pourcentage){
         return keyselected;
     }
@@ -173,6 +167,12 @@ function typing(value){
         console.log(" ✕ "+value);
         erreur ++;
     }
+    if(erreur >= maxerrors) {
+        document.getElementById("gamepage").style.display = "none";
+        document.getElementById("start").style.display = "none";
+        document.getElementById("losepage").style.display = "";
+        document.getElementById("looseresult").innerHTML = listfound.length+"/"+Object.keys(languages).length;
+    }
     gamescorevisual();
 }
 
@@ -180,26 +180,44 @@ async function init(){
     // START
     var start = newElement("div",{"id": "start","style": "background-color: rgba(0, 0, 0, 0.75)"/*"zIndex:0;display:none"*/});
     var logo = newElement("img",{"src":"ressource/Logo2.svg","className":"CenteredImage centerelement"});
+    var logolouse = newElement("img",{"src":"ressource/Logo2.svg","className":"CenteredImage centerelement"});
     var text0 = newElement("p",{"className":"centerelement langcount","style": "font-size:150px;color:#0AEFF7;background: -webkit-linear-gradient(#057C80, #0AEFF7);background-clip: border-box; -webkit-background-clip: text; -webkit-text-fill-color: transparent","innerHTML": "0"});
     var text1 = newElement("p",{"className":"centerelement","style": "font-size:15px;font-family:GothanLight","innerHTML": "Références de<br>langages de programmation<br>à retrouver"});
-    var text = newElement("p",{"innerHTML": "Cliquer ici pour commencée","className":"centerelement btn start0","style": "color:#FFAAFF;margin-top:5vh;display:none"});
-    var text2 = newElement("p",{"innerHTML": "Cliquer ici pour continué","className":"centerelement btn start1","style": "color:#FFAAFF;margin-top:5vh;display:none"});
+    var text2 = newElement("p",{"innerHTML": "Cliquer ici pour commencée","className":"centerelement btn start0","style": "color:#FFAAFF;margin-top:5vh;display:none"});
+    var text3 = newElement("p",{"innerHTML": "Cliquer ici pour continué","className":"centerelement btn start1","style": "color:#FFAAFF;margin-top:5vh;display:none"});
+    var text4 = newElement("p",{"innerHTML": "Cliquer ici pour retourne a l'acceuil","className":"centerelement btn home", "style": "color:#FFAAFF;margin-top:5vh"});
+    var text5 = newElement("p",{"id": "looseresult", "className":"centerelement home", "style": "font-size:150px;color:#0AEFF7;background: -webkit-linear-gradient(#057C80, #0AEFF7);background-clip: border-box; -webkit-background-clip: text; -webkit-text-fill-color: transparent"});
+    var text6 = newElement("p",{"innerHTML": "Votre resultat","className":"centerelement home", "style": "color:#FFAAFF;margin-top:8vh"});
     var charging = newElement("div",{"style": "margin-top:5vh","className":"neon-bar","innerHTML": "<progress class='bar' value='0' max='100'></progress><span class='bar__value'>0%</span>"});
     var typingzone = newElement("div",{"id":"typingzone",'style': 'display:none'});
     var modalgame = newElement("div",{"id":"modalgame",'style': 'display:none'});
+    var losepage = newElement("div",{"id":"losepage",'style': 'display:none',"className":"endpage"});
 
-    text.addEventListener("click", function(){startgame(0)});
-    text2.addEventListener("click", function(){startgame(1)});
+    text2.addEventListener("click", function(){startgame(0)});
+    text3.addEventListener("click", function(){startgame(1)});
+    text4.addEventListener("click", function(){
+        document.getElementById("losepage").style.display = 'none';
+        document.getElementById("gamepage").style.display = 'none';
+        document.getElementById("start").style.display = '';
+        erreur=0;
+        listfound=[];
+        gamestate=0;
+    });
 
     start.appendChild(logo);
     start.appendChild(text0);
     start.appendChild(text1);
-
-    start.appendChild(text);    
-    start.appendChild(text2);
+    start.appendChild(text2);    
+    start.appendChild(text3);
     start.appendChild(charging);
 
+    losepage.appendChild(logolouse);
+    losepage.appendChild(text6);
+    losepage.appendChild(text5);
+    losepage.appendChild(text4);
+
     page.appendChild(start);
+    page.appendChild(losepage);
 
     // Modal Ingame
 
@@ -227,18 +245,18 @@ async function init(){
     .then((json) => {
         return json;
     });
-        var jsonsize = jsonfile.length;
-        for (var i = 0; i < jsonsize-1; i++) {
-            languages[jsonfile[i].name] = {"title": jsonfile[i].name,"description": jsonfile[i].description, "picture": jsonfile[i].picture};
-        }
-        var conditions = newElement("div",{"id":"conditions","style":"display:none","innerHTML":"<h1>"+jsonfile[jsonsize-1].title+"</h1>"+jsonfile[jsonsize-1].content});
-        conditions.appendChild(closebtn());
-        page.appendChild(conditions);
-        var conditonbtn = newElement("span",{"className": "conditonbtn btn","innerHTML": "Conditions General"});
-        conditonbtn.addEventListener("click", function(e) { document.getElementById("conditions").style.display = ""});
-        page.appendChild(conditonbtn);
-    /*});*/
-    // startanimation();
+    var jsonsize = jsonfile.length;
+    for (var i = 0; i < jsonsize-1; i++) {
+        languages[jsonfile[i].name] = {"title": jsonfile[i].name,"description": jsonfile[i].description, "picture": jsonfile[i].picture};
+    }
+    var conditions = newElement("div",{"id":"conditions","style":"display:none","innerHTML":"<h1>"+jsonfile[jsonsize-1].title+"</h1>"+jsonfile[jsonsize-1].content});
+    conditions.appendChild(closebtn());
+    page.appendChild(conditions);
+    var conditonbtn = newElement("span",{"className": "conditonbtn btn","innerHTML": "Conditions General"});
+    conditonbtn.addEventListener("click", function(e) { document.getElementById("conditions").style.display = ""});
+    page.appendChild(conditonbtn);
+
+
     // GAME
     var gamepage = newElement("div",{"id":"gamepage","style":"display:none;zIndex:0"});
     var zoom = newElement("div",{"className": "zoom"});
